bf9a41ec28bbad3b3472e615e1ee32e93cdfc69a
MajemiteJames <majemiteokoro@gmail.com>
2019-08-19 18:13:02 +0100

    third commit

--- api/routes/validation.js
+++ api/routes/validation.js
@@ -4,7 +4,7 @@ import Description from '../controller/task';
 const descriptionRouter = express.Router();
 
 descriptionRouter.post('/', Description.withValidation);
-descriptionRouter.get('/2', Description.withoutValidation);
+descriptionRouter.get('/removeItem', Description.withoutValidation);
 
 
 export default descriptionRouter;
