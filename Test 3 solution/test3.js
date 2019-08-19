e3f4b4d1176cf11b81a569b237df0a043f224369
MajemiteJames <majemiteokoro@gmail.com>
2019-08-19 20:22:30 +0100

    fifth commit

--- Test 3 solution/test3.js
+++ Test 3 solution/test3.js
@@ -1,25 +1,23 @@
-// returns the index as stated in the question.
+/* eslint-disable no-undef */
+/* eslint-disable prefer-const */
+
 LowerIndex = (Magic, dist) => {
-    let responseMagic, responsedist, difference, result;
-    Magic = [3, 2, 5, 4];
-    dist = [4, 3, 1, 3];
-    if (!Array.isArray(Magic) || !Array.isArray(dist)) {
-        return 'input only an array';
-    }
-    responseMagic = Magic.reduce((acc, item) => {
-        return acc + item;
-    }, 0);
-    responsedist = dist.reduce((acc, item) => {
-        return acc + item;
-    }, 0);
-    if (responsedist > responseMagic) {
-        return -1;
-    }
-    else {
-        difference = responseMagic - responsedist;
-        result = Magic.indexOf(difference);
-    }
-    return result;
+  let responseMagic, responsedist, difference, result;
+  Magic = [3, 2, 5, 4];
+  dist = [2, 3, 4, 2];
+  if (!Array.isArray(Magic) || !Array.isArray(dist)) {
+    return 'This function takes in only arrays';
+  }
+  responseMagic = Magic.reduce((acc, item) => acc + item, 0);
+  responsedist = dist.reduce((acc, item) => acc + item, 0);
+  if (responsedist > responseMagic) {
+    return -1;
+  }
+
+  difference = responseMagic - responsedist;
+  result = Magic.indexOf(difference);
+
+  return result;
 };
 
 console.log(LowerIndex());
